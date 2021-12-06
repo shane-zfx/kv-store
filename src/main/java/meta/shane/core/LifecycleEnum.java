6b191d2f177b131514f8777865d805800f371e57
zhangfengxin <Zzfx_7@163.com>
2021-12-06 22:25:38 +0800

    feat<Node>:Schedule

--- src/main/java/meta/shane/core/LifecycleEnum.java
+++ src/main/java/meta/shane/core/LifecycleEnum.java
@@ -6,6 +6,7 @@ package meta.shane.core;
  * @date 2021-12-02 23:58
  */
 public enum LifecycleEnum {
+    INITED,
     STARTED,
     STOPPED,
     CLOSED
--- src/main/java/meta/shane/core/node/Node.java
+++ src/main/java/meta/shane/core/node/Node.java
@@ -1,6 +1,15 @@
 package meta.shane.core.node;
 
 import meta.shane.core.Lifecycle;
+import meta.shane.core.LifecycleEnum;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static meta.shane.core.LifecycleEnum.INITED;
 
 /**
  * 服务节点
@@ -14,18 +23,31 @@ import meta.shane.core.Lifecycle;
  * @date 2021-12-02 23:16
  */
 public final class Node implements Lifecycle {
-    private final RoleEnum role;
+    private LifecycleEnum status;
+    private RoleEnum role;
     /**
      * 所有节点的时钟周期保持一致
      */
-    private final int term;
+    private int term;
 
     public Node() {
         this.role = RoleEnum.FOLLOWER;
         this.term = 0;
+        this.status = INITED;
     }
 
-    private
+    private void t() {
+        // 时钟
+    }
+
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+        service.scheduleAtFixedRate(
+                () -> System.out.println("定时打印，时间 : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))),
+                0,
+                2,
+                TimeUnit.SECONDS);
+    }
 
     @Override
     public void start() {
